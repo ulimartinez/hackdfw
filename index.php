1a4559d9dee33e0ddcf844ab46d2adc315e8ac9c
ulimartinez <ulimartinez96@gmail.com>
2016-04-16 19:58:25 -0600

    edited dashboard buttons

--- index.php
+++ index.php
@@ -117,12 +117,12 @@
                                     <i class="fa fa-shopping-cart fa-5x"></i>
                                 </div>
                                 <div class="col-xs-9 text-right">
-                                    <div class="huge">124</div>
-                                    <div>New Orders!</div>
+                                    <div class="huge">New groceries?</div>
+                                    <div>Update your pantry</div>
                                 </div>
                             </div>
                         </div>
-                        <a href="#">
+                        <a href="mypantry.php">
                             <div class="panel-footer">
                                 <span class="pull-left">View Details</span>
                                 <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
