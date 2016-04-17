17dcc8eea359b15f65e5ff2a734bf6531d4ae71e
Ulises <ulimartinez96@gmail.com>
2016-04-17 12:00:19 -0400

    final version

--- js/MyRecipes.js
+++ js/MyRecipes.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $("img").click(function(){
     var rId = $(this).attr('id');
-    $.post("fetchRecipe.php", {rId: rId}, function(data){
+    $.post("fetchrecipe.php", {rId: rId}, function(data){
       $("#targetDiv_recipe").html(data);
       $("#targetDiv_recipe").fadeIn(300);
       $('#targetDiv_recipe').click();
