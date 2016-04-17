91e050293d60cac085bfc69a38988a3c3a0c0100
ulimartinez <ulimartinez96@gmail.com>
2016-04-17 01:43:17 -0600

    added id authentication by call

--- tropo.php
+++ tropo.php
@@ -7,12 +7,21 @@ function init(){
 	header('Content-Type: application/json');
 	$toReturn = init();
 	$toReturn['tropo'] = array();
-	array_push($toReturn['tropo'], array('ask' => array('say' => [array('value'=>"Hello, this is a test application running on a webserver other than tropo.")])));
+
+	//ask for id
+	array_push($toReturn['tropo'], array('ask' => array('say' => [array('value'=>"Hello, what is your id?")])));
 	$toReturn['tropo'][0]['ask']['required'] = "true";
 	$toReturn['tropo'][0]['ask']['timeout'] = 30;
-	$toReturn['tropo'][0]['ask']['choices'] = array('value' => 'ingredient(milk, eggs, bread, butter, flour, meat)');
-	$toReturn['tropo'][0]['ask']['name'] = "ingredient";
-	array_push($toReturn['tropo'], array('on' => array('event' => 'continue', 'next' => '/tropoResult.php')));
+	$toReturn['tropo'][0]['ask']['choices'] = array('value' => '[4 DIGITS]');
+	$toReturn['tropo'][0]['ask']['name'] = "id";
+
+	//ask for ingredient
+	array_push($toReturn['tropo'], array('ask' => array('say' => [array('value'=>"Hello, this is a test application running on a webserver other than tropo.")])));
+	$toReturn['tropo'][1]['ask']['required'] = "true";
+	$toReturn['tropo'][1]['ask']['timeout'] = 30;
+	$toReturn['tropo'][1]['ask']['choices'] = array('value' => 'ingredient(milk, eggs, bread, butter, flour, meat)');
+	$toReturn['tropo'][1]['ask']['name'] = "ingredient";
+	array_push($toReturn['tropo'], array('on' => array('event' => 'continue', 'next' => 'tropoResult.php')));
 	//$toReturn['tropo']['say']['value'] = "Hello, this is a test application running on a webserver other than tropo.";
 	echo json_encode($toReturn);
 ?>
