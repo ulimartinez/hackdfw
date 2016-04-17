4c7a6f6fd56a468820f83c64af801a5da9e76c52
Ulises <ulimartinez96@gmail.com>
2016-04-17 03:02:35 -0400

    fixed call response

--- tropo.php
+++ tropo.php
@@ -7,7 +7,7 @@ function init(){
 	header('Content-Type: application/json');
 	$toReturn = init();
 	$toReturn['tropo'] = array();
-	array_push($toReturn['tropo'], array('ask' => array('say' => [array('value'=>"Hello, this is a test application running on a webserver other than tropo.")])));
+	array_push($toReturn['tropo'], array('ask' => array('say' => [array('value'=>"Hello, What item did you buy?.")])));
 	$toReturn['tropo'][0]['ask']['required'] = "true";
 	$toReturn['tropo'][0]['ask']['timeout'] = 30;
 	$toReturn['tropo'][0]['ask']['choices'] = array('value' => 'ingredient(milk, eggs, bread, butter, flour, meat)');
